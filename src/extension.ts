2ef886dab0406b711a410fc6df231dc9d527b998
mohankumarelec <mohankumarelec@gmai.com>
2024-01-07 20:49:31 +0530

    Updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "proxy-credential-viewer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('proxy-credential-viewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Proxy Credential Viewer!');
-	});
-
-	context.subscriptions.push(disposable);
+  let disposable = vscode.commands.registerCommand(
+    "proxy-credential-viewer.showProxyCredential",
+    () => {
+      const config = vscode.workspace.getConfiguration("http");
+      const proxy = config.proxyAuthorization as string;
+      const bufferObj = Buffer.from(proxy.split(" ")[1], "base64");
+      const decodedProxy = bufferObj.toString("utf8");
+      const username = decodedProxy.split(":")[0];
+      const password = decodedProxy.split(":")[1];
+      vscode.window.showInformationMessage(`Proxy password: ${password}`);
+      vscode.window.showInformationMessage(`Proxy username: ${username}`);
+    }
+  );
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
